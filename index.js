713557821ca6bc65adb1d52e2ef17b7c7e6423bb
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-04 18:48:42 -0600

    Update index.js
    debug

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const {prompt} = require ("inquirer");
-const { removeEmployee, removeDepartment } = require("./db");
+const {sql} = require ("mysql");
 const db = require("./db");
 require ("console.table");
+
 
 
 init();
@@ -16,27 +17,27 @@ function loadMainPrompts(){
         type: "list",
         name: "option",
         message: "How can I help you?",
-        options [
+        choices: [
             {
                 name: "add employee",
                 value: "ADD_EMPLOYEE"
-            }
+            },
             {
                 name: "delete employee",
                 value:"REMOVE_EMPLOYEE"
-            }
+            },
             {
                 name: "add role",
                 value: "ADD_ROLE"
-            }
+            },
             {
                 name: "delete role",
                 value:"REMOVE_ROLE"
-            }
+            },
             {
                 name: "add department",
                 value: "ADD_DEPARTMENT"
-            }
+            },
             {
                 name: "delete department",
                 value:"REMOVE_DEPARTMENT"
@@ -53,67 +54,72 @@ function loadMainPrompts(){
             case "ADD_EMPLOYEE":
                 addEmployee();
                 break;
-            case "REMOVE_EMPLOYEE"
+            case "REMOVE_EMPLOYEE":
                 removeEmployee();
                 break;
             case "ADD_ROLE":
                 addRole();
                 break;
-            case "REMOVE_ROLE"
+            case "REMOVE_ROLE":
                 removeRole();
                 break;
             case "ADD_DEPARTMENT":
                 addDepartment();
                  break;
-            case "REMOVE_DEPARTMENT"
+            case "REMOVE_DEPARTMENT":
                 removeDepartment();
                 break;
-                
+            case "QUIT":
+                quit();
         }
     })
 }
+
+function addEmployee() {
+        prompt([
+          {
+            name: "firstname",
+            message: "What is the employees name?",
+          },
+          {
+            name: "lastname",
+            message: "What is the employees  last name?",
+          }
+        ])
+          .then(res=> {
+            let fName = res.firstname;
+            let lName = res.lastname;
+          })
+        }
+
 function removeEmployee() {
-    db.findAllEmployees()
-      .then(([rows]) => {
-        let employees = rows;
-        const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
-          name: `${first_name} ${last_name}`,
-          value: id
-        }));
   
         prompt([
           {
             type: "list",
             name: "employeeId",
             message: "Which employee would you like to delete",
-            choices: employeeChoices
           }
         ])
           .then(res => db.removeEmployee(res.employeeId))
           .then(() => console.log("Employee has been deleted"))
           .then(() => loadMainPrompts())
-      })
-  }
+      }
+  
 
   function addRole() {
-    db.findAllDepartments()
-      .then(([rows]) => {
-        let departments = rows;
-        const departmentChoices = departments.map(({ id, name }) => ({
-          name: name,
-          value: id
-        }));
+ 
   
         prompt([
           {
             name: "title",
-            message: "What role would you like to add?"
+            message: "What role would you like to add?",
           },
           {
             type: "list",
             name: "department_id",
             message: "Which department does the role belong to?",
-            choices: departmentChoices
+            
           }
         ])
           .then(role => {
@@ -121,36 +127,28 @@ function removeEmployee() {
               .then(() => console.log("Role has been added."))
               .then(() => loadMainPrompts())
           })
-      })
+      
   }
   
   function removeRole() {
-    db.findAllRoles()
-      .then(([rows]) => {
-        let roles = rows;
-        const roleChoices = roles.map(({ id, title }) => ({
-          name: title,
-          value: id
-        }));
-  
-        prompt([
+          prompt([
           {
             type: "list",
             name: "roleId",
-            message:"what role would you like to delete?"
-            choices: roleChoices
+            message:"what role would you like to delete?",
+           
           }
         ])
           .then(res => db.removeRole(res.roleId))
           .then(() => console.log("Role has been deleted"))
           .then(() => loadMainPrompts())
-      })
+      
   }
   function addDepartment() {
     prompt([
       {
         name: "name",
-        message: "What department would you like to add?"
+        message: "What department would you like to add?",
       }
     ])
       .then(res => {
@@ -163,23 +161,20 @@ function removeEmployee() {
   
   
   function removeDepartment() {
-    db.findAllDepartments()
-      .then(([rows]) => {
-        let departments = rows;
-        const departmentChoices = departments.map(({ id, name }) => ({
-          name: name,
-          value: id
-        }));
-  
+    
         prompt({
           type: "list",
           name: "departmentId",
           message:
             "What department would you like to delete",
-          choices: departmentChoices
+          
         })
           .then(res => db.removeDepartment(res.departmentId))
           .then(() => console.log(`Department has been deleted`))
           .then(() => loadMainPrompts())
-      })
+      
+  }
+  function quit() {
+    console.log("Goodbye!");
+    process.exit();
   }
